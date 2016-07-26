43a7180db6eaae48204f242b6f7556ee2d240096
bubenshchykov <a.bubenshchykov@gmail.com>
2016-07-26 21:53:32 +0800

    payment terms

---
+++ index.js
@@ -0,0 +1,54 @@
+export const types = {
+	Net: 'Net',
+	InvoiceMonth: 'Invoice month',
+	Paid: 'Paid',
+	InvoiceWeekStartingSunday: 'Invoice week, starting Sunday',
+	InvoiceWeekStartingMonday: 'Invoice week, starting Monday',
+	CustomInvoiceDate: 'Custom days from invoice date',
+	CustomMonthEnd: 'Custom days from month end',
+	Fixed: 'Fixed due date'
+};
+
+export const terms = [
+	{
+		id: 1,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_8DAYS',
+		type: types.Net,
+		days: 8
+	},
+	{
+		id: 3,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_14DAYS',
+		type: types.Net,
+		days: 14
+	},
+	{
+		id: 4,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_30DAYS',
+		type: types.Net,
+		days: 30
+	},
+	{
+		id: 5,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_CUSTOM_INVOICE_DATE',
+		shortNameTextId: 'SBA_SLS_DRAFT_INV_CUSTOMER_CREATION_POPUP_CUSTOM_TERMS_FROM_INVOICE_DATE_OPTION',
+		type: types.CustomInvoiceDate
+	},
+	{
+		id: 2,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_PREPAID',
+		type: types.Paid,
+		days: 0
+	},
+	{
+		id: 6,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_CUSTOM_MONTH_END',
+		shortNameTextId: 'SBA_SLS_DRAFT_INV_CUSTOMER_CREATION_POPUP_CUSTOM_TERMS_FROM_MONTH_END_OPTION',
+		type: types.CustomMonthEnd
+	},
+	{
+		id: 7,
+		nameTextId: 'SBA_TERMS_OF_PAYMENT_FIXED',
+		type: types.Fixed
+	}
+];
